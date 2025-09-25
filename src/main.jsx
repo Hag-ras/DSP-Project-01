ad202cd0d3dcf88b112486575d9a5d08b551706d
Hagras <mohamed.hagras04@eng-st.cu.edu.eg>
2025-09-25 14:41:17 +0300

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename="/DSP-Project-01">
       <App />
     </BrowserRouter>
   </StrictMode>
