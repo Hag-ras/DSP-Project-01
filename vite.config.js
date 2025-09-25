16b1c537c8b25167c5b8afb1aa31a04391111db9
Hag-ras <147995461+Hag-ras@users.noreply.github.com>
2025-09-25 17:14:17 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/DSP-Project-01/", 
+  base: "./", 
   build: {
     outDir: "dist",
   },
 });
+
